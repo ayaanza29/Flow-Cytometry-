15c1042069461190907b7e7fb34a070550c946b9
ayaanza29 <91754746+ayaanza29@users.noreply.github.com>
2022-01-19 13:32:08 -0500

    rpy2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #subprocess.call ("/usr/bin/Rscript --vanilla C:/Users/Zuhayr/Documents/GitHub/Flow-Cytometry-/functions.r", shell = True)
 
 
-import rpy2.robjects as robjects
+import rpy2 #.robjects as robjects
 
 "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\R\R x64 4.1.1.lnk"
+"C:\Program Files\R\R-4.1.1\bin\x64"
