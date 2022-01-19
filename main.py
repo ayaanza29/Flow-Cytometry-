fcfc404f1c32909e8b5dfebbe2a0a119aafee7af
ayaanza29 <91754746+ayaanza29@users.noreply.github.com>
2022-01-19 14:38:28 -0500

    changes

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #import subprocess
 #subprocess.call ("/usr/bin/Rscript --vanilla C:/Users/Zuhayr/Documents/GitHub/Flow-Cytometry-/functions.r", shell = True)
 
-
+"""
 import rpy2 #.robjects as robjects
 
 "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\R\R x64 4.1.1.lnk"
 "C:\Program Files\R\R-4.1.1\bin\x64"
+"""
+
+
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rkhan\Documents\GitHub\Flow-Cytometry-\venv
+set VIRTUAL_ENV=C:\Users\Zuhayr\Documents\GitHub\Flow-Cytometry-\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
